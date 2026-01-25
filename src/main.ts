3789e272885848129dad154a700519eda18095a2
Anh Tai <lehuynhanhtai@gmail.com>
2026-01-25 09:43:47 +0700

    feat: setup swagger, class validator

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,44 @@
-
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  // Enable CORS
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Clothes Store API')
+    .setDescription('API documentation for Clothes Store Application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3001);
+
+  Logger.log(`Server running on http://localhost:${process.env.PORT || 3001}`);
+  Logger.log(
+    `Swagger docs available at http://localhost:${process.env.PORT || 3001}/api`,
+  );
 }
 bootstrap();
