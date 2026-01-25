fbb30daa5fa9cae2c70941d0cb1717272d9cfb9a
Anh Tai <lehuynhanhtai@gmail.com>
2026-01-25 10:40:53 +0700

    feat: setup prisma, modules USERS

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  id: string;
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  email: string;
+
+  @ApiProperty()
+  @IsNumber()
+  age: number;
+
+  @ApiProperty()
+  @IsNumber()
+  phone: number;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+}
